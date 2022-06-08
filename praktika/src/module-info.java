ca3de88264f2cf69465d5c95591613169e891a6e
danil1nanatolyi <80408487+danil1nanatolyi@users.noreply.github.com>
2022-06-08 21:28:34 +0300

    Add files via upload

---
+++ praktika/src/module-info.java
@@ -0,0 +1,9 @@
+module Kyrsovaya {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+//    requires org.junit.jupiter.api;
+
+    opens sample;
+}
---
+++ praktika/src/sample/ControllerMainWindow.java
@@ -0,0 +1,61 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class ControllerMainWindow implements Initializable {
+    @FXML TableView<WeatherDay> table;
+    @FXML TableColumn<WeatherDay, Integer> dayColumn;
+    @FXML TableColumn<WeatherDay, Integer> monthColumn;
+    @FXML TableColumn<WeatherDay, Integer> precipitationColumn;
+    @FXML TableColumn<WeatherDay, Integer> temperatureColumn;
+    @FXML TableColumn<WeatherDay, Integer> pressureColumn;
+    @FXML TableColumn<WeatherDay, Integer> humidityColumn;
+
+    ArrayList<WeatherDay> days = new ArrayList<>();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        dayColumn.setCellValueFactory(new PropertyValueFactory<>("day"));
+        monthColumn.setCellValueFactory(new PropertyValueFactory<>("month"));
+        precipitationColumn.setCellValueFactory(new PropertyValueFactory<>("precipitation"));
+        temperatureColumn.setCellValueFactory(new PropertyValueFactory<>("temperature"));
+        pressureColumn.setCellValueFactory(new PropertyValueFactory<>("pressure"));
+        humidityColumn.setCellValueFactory(new PropertyValueFactory<>("humidity"));
+
+        days = getAllDays("test", "SELECT * FROM test.temperatures");
+        table.getItems().addAll(days);
+    }
+
+    public ArrayList<WeatherDay> getAllDays(String nameDB, String sqlQuery){
+        ArrayList<WeatherDay> listDays = new ArrayList<>();
+        try {
+            Connection connection = WeatherDAO.getConnection(nameDB);
+            ResultSet resultSet = WeatherDAO.query(connection, sqlQuery);
+
+            while (resultSet.next()){
+                int day = resultSet.getInt(2);
+                int month = resultSet.getInt(3);
+                int precipitation = resultSet.getInt(4);
+                int temperature = resultSet.getInt(5);
+                int pressure = resultSet.getInt(6);
+                int humidity = resultSet.getInt(7);
+
+                WeatherDay weatherDay = new WeatherDay(day, month, precipitation, temperature, pressure, humidity);
+                listDays.add(weatherDay);
+            }
+        } catch (
+                SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return listDays;
+    }
+}
